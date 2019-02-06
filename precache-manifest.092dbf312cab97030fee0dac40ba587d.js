022d3ba7c006b89cbcd307de9ba8e332404a862f
Arun Chembilath <arun@acrodelon.com>
2019-02-06 13:30:06 +0530

    Updates

---
+++ precache-manifest.092dbf312cab97030fee0dac40ba587d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94c7a48651d9665811bb",
+    "url": "/directorybrowser/static/js/runtime~main.94c7a486.js"
+  },
+  {
+    "revision": "adc884aea5826d74142d",
+    "url": "/directorybrowser/static/js/main.adc884ae.chunk.js"
+  },
+  {
+    "revision": "212dce8348d0c9791292",
+    "url": "/directorybrowser/static/js/1.212dce83.chunk.js"
+  },
+  {
+    "revision": "adc884aea5826d74142d",
+    "url": "/directorybrowser/static/css/main.8b228b1e.chunk.css"
+  },
+  {
+    "revision": "53df88627c2321f2753a160a1cbe1ad1",
+    "url": "/directorybrowser/index.html"
+  }
+];
